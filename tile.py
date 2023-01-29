4a55593e5efaafaca4df61a207a325ac35b1b72a
Veroni <veronika@zisel.ca>
2023-01-28 16:30:27 -0800

    creating main settings of tiles

--- tile.py
+++ tile.py
@@ -1,1 +1,10 @@
+import pygame
+from settings import *
 
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, pos, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load('rock.png').convert_alpha()
+        self.rect = self.image.get_rect(topleft=pos)
+        # offset for better visual effects
+        self.offset_visual = self.rect.inflate(0, -10)
