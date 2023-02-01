0fe220f09accd0d921968a1dc809103e47418a09
Veroni <veronika@zisel.ca>
2023-01-31 19:57:53 -0800

    Update level.py

--- level.py
+++ level.py
@@ -1,1 +1,27 @@
+import pygame
+from settings import *
+from debug import *
+from tile import Tile
+from player import Player
+
+
+class Level:
+    def __init__(self):
+
+        self.display_surface = pygame.display.get_surface()
+        self.visible_sprites = YSortCameraGroup()
+        self.obstacle_sprites = pygame.sprite.Group()
+
+        self.create_map()
+
+    def create_map(self):
+        for row_index, row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'x':
+                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
+
+                if col == 'p':
+                    self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites)
 
