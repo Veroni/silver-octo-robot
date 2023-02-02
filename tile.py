d4381e0bb4cf419c3f7c24caec9bbc25a1e356a7
Veroni <veronika@zisel.ca>
2023-02-02 15:39:18 -0800

    updating tiles

--- tile.py
+++ tile.py
@@ -2,9 +2,16 @@ import pygame
 from settings import *
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
+    def __init__(self, pos, groups, sprite_type, surface=pygame.Surface((TILESIZE, TILESIZE))):
+    #def __init__(self, pos, groups, surface=pygame.Surface((TILESIZE, TILESIZE))):
         super().__init__(groups)
-        self.image = pygame.image.load('rock.png').convert_alpha()
-        self.rect = self.image.get_rect(topleft=pos)
+        self.sprite_type = sprite_type
+        self.image = surface
+        #self.image = pygame.image.load('rock.png').convert_alpha()
+        if sprite_type == 'object':
+            self.rect = self.image.get_rect(topleft=(pos[0], pos[1] - TILESIZE))
+        else:
+            self.rect = self.image.get_rect(topleft=pos)
         # offset for better visual effects
         self.offset_visual = self.rect.inflate(0, -10)
+   
