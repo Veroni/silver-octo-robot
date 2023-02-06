bdcbf8bc4af7858aea1e074749b96f233e60ddbe
Veroni <veronika@zisel.ca>
2023-02-05 23:01:55 -0800

    creating healing system

--- level.py
+++ level.py
@@ -13,6 +13,8 @@ class Level:
         self.visible_sprites = SortCamGroup()
         self.obstacle_sprites = pygame.sprite.Group()
 
+        self.current_attack = None
+
         self.create_map()
 
     def create_map(self):
@@ -43,8 +45,15 @@ class Level:
                             surf = graphics['objects'][int(col)]
                             Tile((x, y), [self.visible_sprites, self.obstacle_sprites], 'object', surf)
 
-        self.player = Player((2000, 1430), [self.visible_sprites], self.obstacle_sprites)
-                    
+        self.player = Player((2000, 1430), [self.visible_sprites], self.obstacle_sprites, self.create_attack, self.destroy_weapon)   
+    def create_attack(self):
+        self.current_attack = Weapon(self.player, [self.visible_sprites])
+
+    def destroy_weapon(self):
+        if self.current_attack:
+            self.current_attack.kill()
+        self.current_attack = None
+    
     def run(self):
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
