4240be29651da3bcf2d3ca99e60c6e2acd3fd238
Veroni <veronika@zisel.ca>
2023-02-14 00:08:31 -0800

    creating health bar

--- ui.py
+++ ui.py
@@ -1,1 +1,9 @@
+import pygame
+from settings import *
 
+class UI:
+    def __init__(self):
+        self.display_surface = pygame.display.get_surface()
+        self.font = pygame.font.Font(UI_FONT, UI_FONT_SIZE)
+        self.health_bar = pygame.Rect(10, 10, HEALTH_BAR_W, BAR_H)
+        self.energy_bar = pygame.Rect(10, 35, ENERGY_BAR_W, BAR_H)
