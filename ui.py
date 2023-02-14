fdad9c8c991d88f1882cf2749eff714c666d1b11
Veroni <veronika@zisel.ca>
2023-02-14 00:12:24 -0800

    displaying health bar

--- ui.py
+++ ui.py
@@ -7,3 +7,9 @@ class UI:
         self.font = pygame.font.Font(UI_FONT, UI_FONT_SIZE)
         self.health_bar = pygame.Rect(10, 10, HEALTH_BAR_W, BAR_H)
         self.energy_bar = pygame.Rect(10, 35, ENERGY_BAR_W, BAR_H)
+    
+    def display(self, player):
+        # for debugging
+        # pygame.draw.rect(self.display_surface, 'black', self.health_bar)
+        self.show_bar(player.health, player.stats['health'],self.health_bar, HEALTH_COL)
+        self.show_bar(player.energy, player.stats['energy'], self.energy_bar, ENERGY_COL)
