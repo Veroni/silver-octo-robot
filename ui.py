86c613842276f6ebe0f1d5db49e0c3d52d582542
Veroni <veronika@zisel.ca>
2023-02-17 18:06:37 -0800

    creating switch remedy icon

--- ui.py
+++ ui.py
@@ -7,6 +7,13 @@ class UI:
         self.font = pygame.font.Font(UI_FONT, UI_FONT_SIZE)
         self.health_bar = pygame.Rect(10, 10, HEALTH_BAR_W, BAR_H)
         self.energy_bar = pygame.Rect(10, 35, ENERGY_BAR_W, BAR_H)
+        
+        # converting remedy dict
+        self.remedy_graphics = []
+        for remedy in weapons.values():
+            path = remedy['graphic']
+            remedy = pygame.image.load(path).convert_alpha()
+            self.remedy_graphics.append(remedy)
           
     def show_experience(self, exp):
         text_surf = self.font.render(str(int(exp)), False, TEXT_EXP_COL)
@@ -22,6 +29,21 @@ class UI:
         current_rect.width = current_w
         pygame.draw.rect(self.display_surface, color, current_rect)
         pygame.draw.rect(self.display_surface, BORDER_COL, current_rect, 2)
+    
+    def selection_box(self, left, top, has_switched):
+        box_rect = pygame.Rect(left, top, ITEM_BAR, ITEM_BAR)
+        pygame.draw.rect(self.display_surface, BG_COL, box_rect)
+        if has_switched:
+            pygame.draw.rect(self.display_surface, BORDER_COL_ACTIVE, box_rect, 2)
+        else:
+            pygame.draw.rect(self.display_surface, BORDER_COL, box_rect, 2)
+        return box_rect
+  
+    def remedy_overlay(self, remedy_index, has_switched):
+        box_rect = self.selection_box(10, 630, has_switched)
+        remedy_surf = self.remedy_graphics[remedy_index]
+        remedy_rect = remedy_surf.get_rect(center=box_rect.center)
+        self.display_surface.blit(remedy_surf, remedy_rect)
         
     def display(self, player):
         # for debugging
@@ -29,3 +51,4 @@ class UI:
         self.show_bar(player.health, player.stats['health'],self.health_bar, HEALTH_COL)
         self.show_bar(player.energy, player.stats['energy'], self.energy_bar, ENERGY_COL)
         self.show_experience(player.exp)
+        self.remedy_overlay(player.remedy_index, not player.can_switch_weapon)
