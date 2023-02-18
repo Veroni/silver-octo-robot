a22cdff5f7a035a54532ce9796c394e6516724f4
Veroni <veronika@zisel.ca>
2023-02-18 13:08:22 -0800

    adding magic tools

--- ui.py
+++ ui.py
@@ -7,20 +7,20 @@ class UI:
         self.font = pygame.font.Font(UI_FONT, UI_FONT_SIZE)
         self.health_bar = pygame.Rect(10, 10, HEALTH_BAR_W, BAR_H)
         self.energy_bar = pygame.Rect(10, 35, ENERGY_BAR_W, BAR_H)
-        
+
         # converting remedy dict
         self.remedy_graphics = []
         for remedy in weapons.values():
             path = remedy['graphic']
             remedy = pygame.image.load(path).convert_alpha()
             self.remedy_graphics.append(remedy)
-          
-    def show_experience(self, exp):
-        text_surf = self.font.render(str(int(exp)), False, TEXT_EXP_COL)
-        x = self.display_surface.get_size()[0] - 15 # offset 15 pix
-        y = self.display_surface.get_size()[1] - 15 # offset 15 pix
-        text_rect = text_surf.get_rect(bottomright=(x,y))
-        
+
+        self.magic_graphics = []
+        for magic in magic_tools.values():
+            path = magic['graphic']
+            magic = pygame.image.load(path).convert_alpha()
+            self.magic_graphics.append(magic)
+
     def show_bar(self, current_amount, max_amount, bg, color):
         pygame.draw.rect(self.display_surface, BG_COL, bg)
         bar_ratio = current_amount / max_amount
@@ -28,8 +28,16 @@ class UI:
         current_rect = bg.copy()
         current_rect.width = current_w
         pygame.draw.rect(self.display_surface, color, current_rect)
-        pygame.draw.rect(self.display_surface, BORDER_COL, current_rect, 2)
-    
+        pygame.draw.rect(self.display_surface, BORDER_COL, bg, 2)
+
+    def show_experience(self, exp):
+        text_surf = self.font.render(str(int(exp)), False, TEXT_EXP_COL)
+        x = self.display_surface.get_size()[0] - 15 # offset 15 pix
+        y = self.display_surface.get_size()[1] - 15 # offset 15 pix
+        text_rect = text_surf.get_rect(bottomright=(x,y))
+
+        self.display_surface.blit(text_surf, text_rect)
+
     def selection_box(self, left, top, has_switched):
         box_rect = pygame.Rect(left, top, ITEM_BAR, ITEM_BAR)
         pygame.draw.rect(self.display_surface, BG_COL, box_rect)
@@ -38,13 +46,19 @@ class UI:
         else:
             pygame.draw.rect(self.display_surface, BORDER_COL, box_rect, 2)
         return box_rect
-  
+
     def remedy_overlay(self, remedy_index, has_switched):
         box_rect = self.selection_box(10, 630, has_switched)
         remedy_surf = self.remedy_graphics[remedy_index]
         remedy_rect = remedy_surf.get_rect(center=box_rect.center)
         self.display_surface.blit(remedy_surf, remedy_rect)
-        
+
+    def magic_overlay(self, magic_index, has_switched):
+        box_rect = self.selection_box(80, 630, has_switched)
+        magic_surf = self.magic_graphics[magic_index]
+        magic_rect = magic_surf.get_rect(center=box_rect.center)
+        self.display_surface.blit(magic_surf, magic_rect)
+
     def display(self, player):
         # for debugging
         # pygame.draw.rect(self.display_surface, 'black', self.health_bar)
@@ -52,3 +66,5 @@ class UI:
         self.show_bar(player.energy, player.stats['energy'], self.energy_bar, ENERGY_COL)
         self.show_experience(player.exp)
         self.remedy_overlay(player.remedy_index, not player.can_switch_weapon)
+        self.magic_overlay(player.magic_index, not player.can_switch_magic)
+        #self.selection_box(80, 630)
