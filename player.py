25b0bb1f2e16472ad0548e7047452099c2eba3f7
Veroni <veronika@zisel.ca>
2023-02-19 20:30:51 -0800

    moving movement and collision to Entity

--- player.py
+++ player.py
@@ -1,9 +1,10 @@
 import pygame
 from settings import *
 from support import import_folder
+from entity import Entity
 
 
-class Player(pygame.sprite.Sprite):
+class Player(Entity):
     def __init__(self, pos, group, obstacle_sprites, create_attack, destroy_weapon, create_magic):
         super().__init__(group)
         self.image = pygame.image.load('player1.png').convert_alpha()
@@ -12,10 +13,7 @@ class Player(pygame.sprite.Sprite):
 
         self.import_player_assets()
         self.state = 'down'
-        self.frame_index = 0
-        self.anim_speed = 0.16
 
-        self.direction = pygame.math.Vector2()
         self.attacking = False
         self.attack_cooldown = 400
         self.attack_time = None
@@ -126,32 +124,6 @@ class Player(pygame.sprite.Sprite):
         else:
             if 'attack' in self.state:
                 self.state = self.state.replace('_attack', '')
-
-    def move(self, speed):
-        if self.direction.magnitude() != 0:
-            self.direction = self.direction.normalize()
-        self.offset_visual.x += self.direction.x * speed
-        self.collision('horizontal')
-        self.offset_visual.y += self.direction.y * speed
-        self.collision('vertical')
-        self.rect.center = self.offset_visual.center
-
-    def collision(self, direction):
-        if direction == 'horizontal':
-            for sprite in self.obstacle_sprites:
-                if sprite.offset_visual.colliderect(self.offset_visual):
-                    if self.direction.x > 0:
-                        self.offset_visual.right = sprite.offset_visual.left
-                    if self.direction.x < 0:
-                        self.offset_visual.left = sprite.offset_visual.right
-
-        if direction == 'vertical':
-            for sprite in self.obstacle_sprites:
-                if sprite.offset_visual.colliderect(self.offset_visual):
-                    if self.direction.y > 0:
-                        self.offset_visual.bottom = sprite.offset_visual.top
-                    if self.direction.y < 0:
-                        self.offset_visual.top = sprite.offset_visual.bottom
 
     def cooldowns(self):
         current_time = pygame.time.get_ticks()
